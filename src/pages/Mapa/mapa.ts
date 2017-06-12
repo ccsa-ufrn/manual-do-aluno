ade4e8e0bea9ecc93162d67164567e3f84288629
Maradona Morais <maradona.morais@hotmail.com>
2017-06-12 16:19:21 -0300

    Google Maps with bug

--- src/pages/Mapa/mapa.ts
+++ src/pages/Mapa/mapa.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 // import { PhotoViewer } from '../../../plugins/com-sarriaroman-photoviewer/www/PhotoViewer';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  LatLng,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+} from '@ionic-native/google-maps';
 
 import { NavController } from 'ionic-angular';
 
@@ -11,9 +20,13 @@ import { Main } from '../Main/main';
 })
 export class Mapa {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private googleMaps: GoogleMaps) { }
 
+  ngAfterViewInit() {
+    this.loadMap();
   }
+
+  loadMap() {}
 
   public goToMain() {
       this.navCtrl.setRoot(Main);
